cdd0fc105bd09d9a54d79436bd68b19cbdce81a9
Johann Kropf <johann.kropf@gmx.at>
2013-08-28 14:24:37 +0200

    string to text

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130723133911) do
+ActiveRecord::Schema.define(:version => 20130828122005) do
 
   create_table "hardwares", :force => true do |t|
     t.string   "typehw"
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(:version => 20130723133911) do
   create_table "servicelevels", :force => true do |t|
     t.string   "level"
     t.string   "short_desc"
-    t.string   "long_desc"
+    t.text     "long_desc"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
