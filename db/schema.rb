3433fe2888066afb3eb2fb9a96792767a467945e
Johann Kropf <johann.kropf@gmx.at>
2014-02-11 22:22:49 +0100

    Make a basic User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130828122005) do
+ActiveRecord::Schema.define(:version => 20140211204906) do
 
   create_table "hardwares", :force => true do |t|
     t.string   "typehw"
@@ -55,6 +55,14 @@ ActiveRecord::Schema.define(:version => 20130828122005) do
     t.text     "long_desc"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
 
   create_table "vendors", :force => true do |t|
