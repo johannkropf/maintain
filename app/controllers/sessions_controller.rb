6752f6abb4d2dd6d10720d4e15cf3a77ffc3a7a8
Johann Kropf <johann.kropf@gmx.at>
2014-02-23 20:07:44 +0100

    authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,15 +5,19 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      #session[:user_id] = user.id
+      session[:user_id] = user.id
       redirect_to hwservices_url, :notice => "Logged in!"
     else
+      reset_session
       flash.now[:danger] = "Invalid email or password"
       render "new"
     end
   end
 
   def destroy
+    reset_session
+    session[:user_id] = nil
+    redirect_to root_url, :notice => "Logged out!"
   end
 
 end
